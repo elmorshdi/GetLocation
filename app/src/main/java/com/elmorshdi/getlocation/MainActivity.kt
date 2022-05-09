9c7f377d627b96fc1e2beb37294606540189fbf3
elmorshdi <elmorshdi53@gmail.com>
2022-05-09 16:06:53 +0200

    show position

--- app/src/main/java/com/elmorshdi/getlocation/MainActivity.kt
+++ app/src/main/java/com/elmorshdi/getlocation/MainActivity.kt
@@ -4,6 +4,7 @@ import android.Manifest
 import android.content.Intent
 import android.content.pm.PackageManager
 import android.os.Bundle
+import android.widget.TextView
 import android.widget.Toast
 import androidx.appcompat.app.AppCompatActivity
 import androidx.appcompat.widget.AppCompatButton
@@ -13,11 +14,17 @@ import androidx.core.content.ContextCompat
 
 class MainActivity : AppCompatActivity() {
     private lateinit var button: AppCompatButton
+    private lateinit var textView: TextView
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
         title = "My Map"
+        textView=findViewById(R.id.location)
         button=findViewById(R.id.get_button)
+
+            if(intent.getStringExtra("LOCATION")!=null) {
+
+            textView.text=intent.getStringExtra("LOCATION")}
         button.setOnClickListener {
             if (ContextCompat.checkSelfPermission(this@MainActivity,android.Manifest.permission.ACCESS_FINE_LOCATION) != PackageManager.PERMISSION_GRANTED) {
                 if (ActivityCompat.shouldShowRequestPermissionRationale(this@MainActivity,
--- app/src/main/java/com/elmorshdi/getlocation/MapActivity.kt
+++ app/src/main/java/com/elmorshdi/getlocation/MapActivity.kt
@@ -3,23 +3,25 @@ package com.elmorshdi.getlocation
 import android.annotation.SuppressLint
 import android.content.Intent
 import android.location.Location
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.os.Looper
+import androidx.appcompat.app.AppCompatActivity
 import androidx.core.location.LocationManagerCompat.requestLocationUpdates
 import com.google.android.gms.location.*
 import com.google.android.gms.maps.CameraUpdateFactory
 import com.google.android.gms.maps.GoogleMap
+import com.google.android.gms.maps.GoogleMap.OnMarkerDragListener
 import com.google.android.gms.maps.OnMapReadyCallback
 import com.google.android.gms.maps.SupportMapFragment
 import com.google.android.gms.maps.model.LatLng
 import com.google.android.gms.maps.model.Marker
 import com.google.android.gms.maps.model.MarkerOptions
 
+
 class MapActivity : AppCompatActivity() , OnMapReadyCallback ,
     GoogleMap.OnMarkerClickListener
 {
-   private lateinit var loca:Location
+  // private lateinit var loca:Location
     private lateinit var mMap: GoogleMap
     private lateinit var fusedLocationClient: FusedLocationProviderClient
     private var locationCallback: LocationCallback? = null
@@ -27,21 +29,21 @@ class MapActivity : AppCompatActivity() , OnMapReadyCallback ,
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_map)
-           val mapFragment = supportFragmentManager
+        val mapFragment = supportFragmentManager
             .findFragmentById(R.id.map) as SupportMapFragment
         mapFragment.getMapAsync(this)
         fusedLocationClient = LocationServices.getFusedLocationProviderClient(this)
 
     }
 
-
     override fun onMapReady(googleMap: GoogleMap) {
         mMap = googleMap
         mMap.uiSettings.isZoomControlsEnabled = true
         mMap.setOnMarkerClickListener(this)
-          getLastKnownLocation {
+
+        getLastKnownLocation {
             val sydney = LatLng(it.latitude, it.longitude)
-              loca.set(it)
+              //loca.set(it)
             mMap.addMarker(
                     MarkerOptions()
                         .position(sydney)
@@ -50,11 +52,30 @@ class MapActivity : AppCompatActivity() , OnMapReadyCallback ,
 
         }
 
+            mMap.setOnMarkerDragListener(object : OnMarkerDragListener {
+    override fun onMarkerDragStart(marker: Marker) {
+        // TODO Auto-generated method stub
     }
 
+    override fun onMarkerDragEnd(marker: Marker) {
+        // TODO Auto-generated method stub
+        mMap.addMarker(
+            MarkerOptions()
+                .position(marker.position)
+                .title("Marker in Sydney"))
+        mMap.moveCamera(CameraUpdateFactory.newLatLngZoom(marker.position,8.0f))
+
+    }
+
+    override fun onMarkerDrag(marker: Marker) {
+        // TODO Auto-generated method stub
+    }
+})
+
+}
     override fun onMarkerClick(p0: Marker): Boolean {
         val intent= Intent(applicationContext,MainActivity::class.java)
-        intent.putExtra("LOCATION","latitude:${loca.latitude} , longitude:${loca.longitude}")
+        intent.putExtra("LOCATION","latitude:${p0.position.latitude} , longitude:${p0.position.longitude}")
         startActivity(intent)
         return true
     }
